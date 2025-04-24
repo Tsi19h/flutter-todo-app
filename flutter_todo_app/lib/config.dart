2e501efb4ebd3831129e017b60bf7d4c9484a399
Tsi19h <tsigeredaaku@gmail.com>
2025-04-24 04:54:52 +0300

    flutter todo app backend and flutter frontend added

---
+++ flutter_todo_app/lib/config.dart
@@ -0,0 +1,7 @@
+final url = 'http://127.0.0.1:3000/';
+final registration = url + "register";
+final login = url + 'login';
+final addtodo = url + 'createToDo';
+final getToDoList = url + 'getUserTodoList';
+final deleteTodo = url + 'deleteTodo';
+final updateTodo = url + 'update';
